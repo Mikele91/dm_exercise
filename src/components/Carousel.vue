fc9bcf15f7673ae07d3f177042ed131c66aab5d2
Mikele91 <mke_8@hotmail.it>
2022-07-20 14:45:29 +0200

    fix

--- src/components/Carousel.vue
+++ src/components/Carousel.vue
@@ -15,14 +15,14 @@
                 </div>
             </div>
             <div class=" d-flex justify-content-between align-items-center">
-                <div class="job">
+                <div class="job_description">
                     <span class="">Founder, Catalog</span>
                     <br>
                     <span>Web Design Agency</span>
                 </div>
                 <div id="arrows_box">
-                    <img @click="nextImg()" class="arrow" src="../assets/img/arrow-l.svg" alt="">
-                    <img @click="prevImg()" class="arrow  ms-4" src="../assets/img/arrow-r.svg" alt="">
+                    <img @click="nextImg(), stop()" class="arrow" src="../assets/img/arrow-l.svg" alt="">
+                    <img @click="prevImg(), stop()" class="arrow  ms-4" src="../assets/img/arrow-r.svg" alt="">
                 </div>
             </div>
         </div>
@@ -34,9 +34,13 @@ export default {
     name:"Carousel",
     data(){
         return{
+        time:null,
         count:0,
         bgImage:[require("../assets/slideshow/1.jpeg"),require("../assets/slideshow/2.jpeg"),require("../assets/slideshow/3.jpeg")],
         }
+    },
+    mounted: function() {
+        this.time=setInterval(()=>this.nextImg() , 5000);
     },
     methods:{
         nextImg(){
@@ -51,6 +55,10 @@ export default {
                 this.count = this.bgImage.length-1;
             }
         },
+        stop: function() {
+            clearInterval(this.time);
+            this.time=setInterval(()=>this.nextImg() , 5000);
+        },
     },
 }
 </script>
@@ -61,51 +69,51 @@ export default {
         overflow-y: auto;
         background-size: cover;
         background-position:center ;
-    }
-    .card_container{
-        display: flex;
-        flex-direction: column;
-        justify-content: flex-end;
-        height: 100%;
-        margin: 0;
-        color: white;
-        background-size: cover;
-        background-position:center ;
-        padding: 2rem 3.125rem;
-        .text_img,#name_img{
-            
-            font-size: 1.5rem;
-            font-weight: 400;
-            text-align: start;
-        }
-        .star_container{
-            img{
-                width:1.25rem; 
-                
-            } 
-        }
-        .arrow{
-
-            border: 1px solid white;
-            border-radius: 50%;
-            width: 3rem;
-            padding: 0.6rem;
-        }
-        .arrow:hover{
-            cursor: pointer;
-        }
-        .job {
-            span{
-                font-size: .75rem;
-            }
-            span:last-child{
-            font-weight: 300;
-            font-size: .65rem;
-
-            }
-        }
-    }
     
+        .card_container{
+            display: flex;
+            flex-direction: column;
+            justify-content: flex-end;
+            height: 100%;
+            margin: 0;
+            color: white;
+            background-size: cover;
+            background-position:center ;
+            padding: 2rem 3.125rem;
+            .text_img,#name_img{
+                
+                font-size: 1.5rem;
+                font-weight: 400;
+                text-align: start;
+            }
+            .star_container{
+                img{
+                    width:1.25rem; 
+                    
+                } 
+            }
+            .arrow{
+
+                border: 1px solid white;
+                border-radius: 50%;
+                width: 3rem;
+                padding: 0.6rem;
+            }
+            .arrow:hover{
+                cursor: pointer;
+            }
+            .job_description {
+                span{
+                    font-size: .75rem;
+                }
+                span:last-child{
+                font-weight: 300;
+                font-size: .65rem;
+
+                }
+            }
+        }
+    }
 
 </style>
 
